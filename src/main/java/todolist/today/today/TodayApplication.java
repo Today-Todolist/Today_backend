09a075490837720e01551a6aae291c1a69d76765
dohyeon5626 <kodohyeon71@gmail.com>
2021-12-28 10:22:43 +0900

    ⚙️ (#1) - Project 기본 Setting 및 Gradle 추가

---
+++ src/main/java/todolist/today/today/TodayApplication.java
@@ -0,0 +1,13 @@
+package todolist.today.today;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayApplication.class, args);
+    }
+
+}
