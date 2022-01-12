8faf15a294fb1c92263c8f06550ccb44b2419a00
dohyeon5626 <kodohyeon71@gmail.com>
2022-01-12 22:39:57 +0900

    ⚙️ (#4) - Json Error Response, Valid Error Response 추가
    Json 관련 에러 메시지를 담당하는 Response와 Valid 관련 에러 메시지를 담당하는 Response 추가

---
+++ src/main/java/todolist/today/today/global/error/dto/ValidErrorResponse.java
@@ -0,0 +1,31 @@
+package todolist.today.today.global.error.dto;
+
+import lombok.Getter;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import todolist.today.today.global.error.ErrorCode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class ValidErrorResponse extends BasicErrorResponse {
+
+    private final Map<String, String> reason;
+
+    private ValidErrorResponse(MethodArgumentNotValidException e) {
+        super(ErrorCode.MISSING_REQUEST);
+
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        this.reason = new HashMap<>(fieldErrors.size());
+        e.getFieldErrors().forEach(fieldError -> {
+            reason.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+    }
+
+    public static ValidErrorResponse from(MethodArgumentNotValidException e) {
+        return new ValidErrorResponse(e);
+    }
+
+}
