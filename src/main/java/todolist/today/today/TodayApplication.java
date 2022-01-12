b0c477733991fe4f46a0e09ef7d6ff270de4184e
dohyeon5626 <kodohyeon71@gmail.com>
2022-01-12 22:37:39 +0900

    ⚙️ (#4) - 지역 설정 추가
    Valid가 제공하는 메시지를 영어로 얻기 위한 지역 설정

--- src/main/java/todolist/today/today/TodayApplication.java
+++ src/main/java/todolist/today/today/TodayApplication.java
@@ -3,10 +3,13 @@ package todolist.today.today;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class TodayApplication {
 
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("en", "us"));
         SpringApplication.run(TodayApplication.class, args);
     }
 
