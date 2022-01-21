5d82da5fa7185f679158a1ed584085d1d6090627
dohyeon5626 <kodohyeon71@gmail.com>
2022-01-22 00:44:03 +0900

    ✅ (#3) - querydsl 버전 하향
    잘못된 import로 인한 오류 해결

--- src/main/java/todolist/today/today/global/entity/User.java
+++ src/main/java/todolist/today/today/global/entity/User.java
@@ -1,13 +1,14 @@
 package todolist.today.today.global.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Getter
 @Builder @NoArgsConstructor @AllArgsConstructor
