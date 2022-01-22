a7f60ed26a63378c2e978951d3438478bf6e387c
dohyeon5626 <kodohyeon71@gmail.com>
2022-01-22 22:25:51 +0900

    ⚙️ (#3) - Jwt Properties 코드 추가
    yml에 있는 정보를 가져오는 코드 추가

---
+++ src/main/java/todolist/today/today/global/security/JwtProperties.java
@@ -0,0 +1,25 @@
+package todolist.today.today.global.security;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import java.util.Base64;
+
+@Getter
+@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    private final String secret;
+    private final Long accessExp;
+    private final Long refreshExp;
+
+    public JwtProperties(String secret, Long accessExp, Long refreshExp) {
+        System.out.println("테스트 " + secret + accessExp + refreshExp);
+        this.secret = Base64.getEncoder().encodeToString(secret.getBytes());
+        this.accessExp = accessExp;
+        this.refreshExp = refreshExp;
+    }
+
+}
