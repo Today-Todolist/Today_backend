3e3b6a01322d9c2200968dd35295e093d6a3a67a
dohyeon5626 <kodohyeon71@gmail.com>
2022-01-22 22:30:31 +0900

    ⚙️ (#3) - Jwt Token Filter 코드 추가
    요청의 인증 정보를 담아줄 filter 코드와 filter에서 사용하는 jwt token provider 코드 추가

---
+++ src/main/java/todolist/today/today/global/security/JwtTokenFilter.java
@@ -0,0 +1,29 @@
+package todolist.today.today.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        jwtTokenProvider.resolveToken(request)
+                .map(jwtTokenProvider::getBody)
+                .filter(jwtTokenProvider::isAccess)
+                .map(jwtTokenProvider::getId)
+                .map(jwtTokenProvider::getAuthentication)
+                .ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
+        chain.doFilter(request,response);
+    }
+
+}
---
+++ src/main/java/todolist/today/today/global/security/JwtTokenProvider.java
@@ -0,0 +1,73 @@
+package todolist.today.today.global.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import todolist.today.today.global.security.auth.AuthDetailsService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final JwtProperties jwtProperties;
+    private final AuthDetailsService authDetailsService;
+
+    public String generateAccessToken(String id) {
+        return makingToken(id, "access", jwtProperties.getAccessExp());
+    }
+
+    public String generateRefreshToken(String id) {
+        return makingToken(id, "refresh", jwtProperties.getRefreshExp());
+    }
+
+    private String makingToken(String id, String type, Long time){
+        return Jwts.builder()
+                .setExpiration(new Date(System.currentTimeMillis() + (time * 1000L)))
+                .signWith(SignatureAlgorithm.HS512, jwtProperties.getSecret())
+                .setIssuedAt(new Date())
+                .setSubject(id)
+                .claim("type", type)
+                .compact();
+    }
+
+    public Optional<String> resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return Optional.of(bearerToken.substring(7));
+        }
+        return Optional.empty();
+    }
+
+    public Authentication getAuthentication(String id) {
+        UserDetails details = authDetailsService.loadUserByUsername(id);
+        return new UsernamePasswordAuthenticationToken(details, "", details.getAuthorities());
+    }
+
+    public Claims getBody(String token) {
+        try {
+            return Jwts.parser().setSigningKey(jwtProperties.getSecret()).parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public boolean isAccess(Claims body) {
+        return body.get("type", String.class).equals("access");
+    }
+
+    public String getId(Claims body) {
+        return body.getSubject();
+    }
+
+
+}
