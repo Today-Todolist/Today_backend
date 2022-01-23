2617645ba22f9c469e0cb7bf201b938379463a27
dohyeon5626 <kodohyeon71@gmail.com>
2022-01-23 23:55:21 +0900

    ⚙️ (#3) - Web Mvc Config 추가
    Cors를 설정한 Web Mvc Config 추가

---
+++ src/main/java/todolist/today/today/global/security/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package todolist.today.today.global.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*");
+    }
+
+}
