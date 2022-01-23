ea56b28b58ef170b95ea4bc3bdc7d7bfe362f63c
dohyeon5626 <kodohyeon71@gmail.com>
2022-01-23 21:27:48 +0900

    ⚙️ (#3) - Request Limit Filter 코드 추가
    요청 수 제한을 위한 필터 추가

--- src/main/java/todolist/today/today/global/error/ErrorCode.java
+++ src/main/java/todolist/today/today/global/error/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "S001", "Internal Server Error"),
 
     INVALID_TOKEN(401, "A001", "Invalid Token"),
+    TOO_MANY_REQUEST(429, "A002", "Too Many Request"),
 
     NOT_IN_JSON_FORMAT(415, "C001", "Not In Json Format"),
     MISSING_REQUEST(400, "C002", "Missing Request"),
---
+++ src/main/java/todolist/today/today/global/security/RequestBucketProvider.java
@@ -0,0 +1,31 @@
+package todolist.today.today.global.security;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RequestBucketProvider {
+
+    private final Map<String, Bucket> requestBuckets = new ConcurrentHashMap<>();
+
+    private String getClientIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        return ip != null ? ip : request.getRemoteAddr();
+    }
+
+    public Bucket resolveBucket(HttpServletRequest httpServletRequest) {
+        return requestBuckets.putIfAbsent(getClientIp(httpServletRequest), newBucket());
+    }
+
+    private Bucket newBucket() {
+        return Bucket.builder()
+                .addLimit(Bandwidth.classic(100, Refill.greedy(20, Duration.ofMinutes(1))))
+                .build();
+    }
+
+}
