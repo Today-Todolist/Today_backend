75edf385331502a08dbb1ef8f24619fc49cdb70d
dohyeon5626 <kodohyeon71@gmail.com>
2022-01-29 00:21:07 +0900

    ♻️ (#7) - 불필요한 import 삭제
    InvalidTokenException 클래스의 불필요한 import 삭제

--- src/main/java/todolist/today/today/global/error/exception/security/InvalidTokenException.java
+++ src/main/java/todolist/today/today/global/error/exception/security/InvalidTokenException.java
@@ -1,7 +1,6 @@
 package todolist.today.today.global.error.exception.security;
 
 import todolist.today.today.global.error.ErrorCode;
-import todolist.today.today.global.error.dto.BasicErrorResponse;
 import todolist.today.today.global.error.exception.BasicException;
 
 public class InvalidTokenException extends BasicException {
