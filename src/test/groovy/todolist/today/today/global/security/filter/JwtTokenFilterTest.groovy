6a45f1c248d1dcefd723c60d73daae979bf1bde1
dohyeon5626 <kodohyeon71@gmail.com>
2022-01-30 21:54:59 +0900

    🧪 (#14) - JwtTokenFilter 테스트 코드 추가
    JwtTokenFilter 테스트 코드 작성 (JwtProvider는 다른 곳에서도 쓰여서 따로 테스트 코드를 작성할 예정)

---
+++ src/test/groovy/todolist/today/today/global/security/filter/JwtTokenFilterTest.groovy
@@ -0,0 +1,66 @@
+package todolist.today.today.global.security.filter
+
+import io.jsonwebtoken.Claims
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import spock.lang.Specification
+import todolist.today.today.global.error.exception.security.InvalidTokenException
+import todolist.today.today.global.security.JwtTokenProvider
+
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtTokenFilterTest extends Specification {
+
+    private JwtTokenFilter jwtTokenFilter
+    private JwtTokenProvider jwtTokenProvider = Stub(JwtTokenProvider)
+    private HttpServletRequest request = Stub(HttpServletRequest)
+    private HttpServletResponse response = Stub(HttpServletResponse)
+    private FilterChain chain = Stub(FilterChain)
+    private Claims body = Stub(Claims)
+    private Authentication authentication = Stub(Authentication)
+
+    def setup() {
+        jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider)
+    }
+
+    def "test tokenExists" () {
+        given:
+        jwtTokenProvider.resolveToken(request) >> Optional.of("token")
+        jwtTokenProvider.getBody("token") >> body
+        jwtTokenProvider.getAuthentication(body) >> authentication
+
+        when:
+        jwtTokenFilter.doFilterInternal(request, response, chain)
+
+        then:
+        SecurityContextHolder.getContext().getAuthentication() == authentication
+    }
+
+    def "test tokenNonExists" () {
+        given:
+        jwtTokenProvider.resolveToken(request) >> Optional.empty()
+
+        when:
+        jwtTokenFilter.doFilterInternal(request, response, chain)
+
+        then:
+        SecurityContextHolder.getContext().getAuthentication() != authentication
+    }
+
+    def "test tokenExists but NotAccess" () {
+        given:
+        jwtTokenProvider.resolveToken(request) >> Optional.of("token")
+        jwtTokenProvider.getBody("token") >> body
+        jwtTokenProvider.getAuthentication(body) >> { throw new InvalidTokenException()}
+
+        when:
+        jwtTokenFilter.doFilterInternal(request, response, chain)
+
+        then:
+        thrown(InvalidTokenException)
+        SecurityContextHolder.getContext().getAuthentication() != authentication
+    }
+
+}
