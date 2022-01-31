68c69fd2c598fbe49f8db3a91b2f5d7e19341f81
dohyeon5626 <kodohyeon71@gmail.com>
2022-01-31 22:09:34 +0900

    ♻️ (#14) - User 코드 변경
    changePossible 자료형 변경

--- src/main/java/todolist/today/today/global/entity/User.java
+++ src/main/java/todolist/today/today/global/entity/User.java
@@ -28,7 +28,6 @@ public class User {
     @Column(nullable = false)
     private String profile;
 
-    @Column(nullable = false)
-    private String changePossible;
+    private boolean changePossible;
 
 }
