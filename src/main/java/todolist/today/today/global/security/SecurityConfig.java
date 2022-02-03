5462362a56c3492b1627cbf55878ae54017e61f1
dohyeon5626 <kodohyeon71@gmail.com>
2022-02-03 20:40:15 +0900

    ⚙️ (#12) - 인증 API 설정 추가
    Spring Security 경로 설정을 어노테이션으로 하기 위한 설정

--- src/main/java/todolist/today/today/global/security/SecurityConfig.java
+++ src/main/java/todolist/today/today/global/security/SecurityConfig.java
@@ -4,18 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import todolist.today.today.global.security.filter.FilterConfig;
-import todolist.today.today.global.security.path.ApiPath;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -26,27 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .sessionManagement().disable()
-                .formLogin().disable();
-
-        for(String path : ApiPath.getCertifyPath(HttpMethod.GET))
-            http.authorizeRequests().antMatchers(HttpMethod.GET, path);
-
-        for(String path : ApiPath.getCertifyPath(HttpMethod.POST))
-            http.authorizeRequests().antMatchers(HttpMethod.POST, path);
-
-        for(String path : ApiPath.getCertifyPath(HttpMethod.PATCH))
-            http.authorizeRequests().antMatchers(HttpMethod.PATCH, path);
-
-        for(String path : ApiPath.getCertifyPath(HttpMethod.PUT))
-            http.authorizeRequests().antMatchers(HttpMethod.PUT, path);
-
-        for(String path : ApiPath.getCertifyPath(HttpMethod.DELETE))
-            http.authorizeRequests().antMatchers(HttpMethod.DELETE, path);
-
-        http.authorizeRequests()
+                .formLogin().disable()
+                .cors().and()
+                .authorizeRequests()
                 .anyRequest().permitAll()
-                .and()
-                .apply(new FilterConfig(jwtTokenProvider, requestBucketProvider, objectMapper));
+                .and().apply(new FilterConfig(jwtTokenProvider, requestBucketProvider, objectMapper));
     }
 
     @Bean
