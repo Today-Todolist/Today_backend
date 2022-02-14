0764a202cc105fc214087815d9856c2538b4fe47
dohyeon5626 <kodohyeon71@gmail.com>
2022-02-14 12:17:52 +0900

    🧪 (#20) - MailContentProvider 테스트 코드 추가
    MailContentProvider 테스트 코드 추가

---
+++ src/test/groovy/todolist/today/today/infra/mail/MailContentProviderTest.groovy
@@ -0,0 +1,42 @@
+package todolist.today.today.infra.mail
+
+import spock.lang.Specification
+import todolist.today.today.infra.mail.properties.MailProperties
+
+
+class MailContentProviderTest extends Specification {
+
+    private MailContentProvider mailContentProvider
+    private MailProperties mailProperties = Stub(MailProperties)
+    private final String SIGN_UP_LINK = "https://signup"
+    private final String CHANGE_PASSOWRD_LINK = "https://changepassword"
+
+    def setup() {
+        mailProperties.getSignUpLink() >> SIGN_UP_LINK
+        mailProperties.getChangePasswordLink() >> CHANGE_PASSOWRD_LINK
+        mailContentProvider = new MailContentProvider(mailProperties)
+    }
+
+    def "test createSignUpContent" () {
+        given:
+        final String TOKEN = "1234"
+
+        when:
+        String content = mailContentProvider.createSignUpContent(TOKEN)
+
+        then:
+        content == SIGN_UP_LINK + "?token=" + TOKEN
+    }
+
+    def "test createChangePasswordContent" () {
+        given:
+        final String TOKEN = "1234"
+
+        when:
+        String content = mailContentProvider.createChangePasswordContent(TOKEN)
+
+        then:
+        content == CHANGE_PASSOWRD_LINK + "?token=" + TOKEN
+    }
+
+}
