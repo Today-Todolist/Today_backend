f74a418eb35abf8207724594ac925b85585ab63f
dohyeon5626 <kodohyeon71@gmail.com>
2022-03-03 13:19:49 +0900

    🧪 (#37) - FriendInfoService 테스트 코드 추가
    FriendInfoService 테스트 코드 추가

---
+++ src/test/groovy/todolist/today/today/domain/friend/application/FriendInfoServiceTest.groovy
@@ -0,0 +1,69 @@
+package todolist.today.today.domain.friend.application
+
+import spock.lang.Specification
+import todolist.today.today.domain.friend.dao.CustomFriendApplyRepositoryImpl
+import todolist.today.today.domain.friend.dao.CustomFriendRepositoryImpl
+import todolist.today.today.domain.friend.dao.FriendApplyRepository
+import todolist.today.today.domain.friend.dao.FriendRepository
+import todolist.today.today.domain.friend.dto.response.UserFriendApplyResponse
+import todolist.today.today.domain.friend.dto.response.UserFriendResponse
+import todolist.today.today.global.dto.request.PagingRequest
+import todolist.today.today.global.dto.response.PagingResponse
+
+
+class FriendInfoServiceTest extends Specification {
+
+    private FriendInfoService friendInfoService
+    private CustomFriendRepositoryImpl customFriendRepository = Mock(CustomFriendRepositoryImpl)
+    private CustomFriendApplyRepositoryImpl customFriendApplyRepository = Mock(CustomFriendApplyRepositoryImpl)
+    private FriendRepository friendRepository = Stub(FriendRepository)
+    private FriendApplyRepository friendApplyRepository = Stub(FriendApplyRepository)
+
+    def setup() {
+        friendInfoService = new FriendInfoService(customFriendRepository,
+                customFriendApplyRepository,
+                friendRepository,
+                friendApplyRepository)
+    }
+
+    def "test getUserFriends" () {
+        given:
+        final String USER_ID = "today043149@gmail.com"
+        PagingRequest request = new PagingRequest(0, 1)
+
+        friendRepository.countByFriendEmailOrUserEmail(USER_ID, USER_ID) >> count
+
+        when:
+        PagingResponse<UserFriendResponse> response = friendInfoService.getUserFriends(USER_ID, USER_ID, request)
+
+        then:
+        number * customFriendRepository.getUserFriends(USER_ID, USER_ID, request)
+        response.getTotalElements() == count
+
+        where:
+        count || number
+        0L || 0
+        1L || 1
+    }
+
+    def "test getUserFriendsApply" () {
+        given:
+        final String USER_ID = "today043149@gmail.com"
+        PagingRequest request = new PagingRequest(0, 1)
+
+        friendApplyRepository.countByFriendEmailOrUserEmail(USER_ID, USER_ID) >> count
+
+        when:
+        PagingResponse<UserFriendApplyResponse> response = friendInfoService.getUserFriendsApply(USER_ID, request)
+
+        then:
+        number * customFriendApplyRepository.getUserFriendApply(USER_ID, request)
+        response.getTotalElements() == count
+
+        where:
+        count || number
+        0L || 0
+        1L || 1
+    }
+
+}
