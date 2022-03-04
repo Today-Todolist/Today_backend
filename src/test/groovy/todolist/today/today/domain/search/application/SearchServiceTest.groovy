54997a791b4ba43de7e3d00eea663abef4bc4ab3
dohyeon5626 <kodohyeon71@gmail.com>
2022-03-04 16:55:14 +0900

    🧪 (#43) - SearchService 테스트 코드 추가
    SearchService 테스트 코드 추가

---
+++ src/test/groovy/todolist/today/today/domain/search/application/SearchServiceTest.groovy
@@ -0,0 +1,79 @@
+package todolist.today.today.domain.search.application
+
+import spock.lang.Specification
+import todolist.today.today.domain.search.dao.CustomSearchWordRepositoryImpl
+import todolist.today.today.domain.search.dao.SearchWordRepository
+import todolist.today.today.domain.search.domain.SearchWord
+import todolist.today.today.domain.template.dao.CustomTemplateRepositoryImpl
+import todolist.today.today.domain.template.dao.TemplateRepository
+import todolist.today.today.domain.user.dao.CustomUserRepositoryImpl
+import todolist.today.today.domain.user.dao.UserRepository
+import todolist.today.today.global.dto.request.PagingRequest
+
+class SearchServiceTest extends Specification {
+
+    private SearchService searchService
+    private CustomSearchWordRepositoryImpl customSearchWordRepository = Stub(CustomSearchWordRepositoryImpl)
+    private CustomUserRepositoryImpl customUserRepository = Stub(CustomUserRepositoryImpl)
+    private CustomTemplateRepositoryImpl customTemplateRepository = Stub(CustomTemplateRepositoryImpl)
+    private SearchWordRepository searchWordRepository = Stub(SearchWordRepository)
+    private UserRepository userRepository = Stub(UserRepository)
+    private TemplateRepository templateRepository = Stub(TemplateRepository)
+
+    def setup() {
+        searchService = new SearchService(customSearchWordRepository,
+                customUserRepository,
+                customTemplateRepository,
+                searchWordRepository,
+                userRepository,
+                templateRepository)
+    }
+
+    def "test getSearchWord" () {
+        when:
+        searchService.getSearchWord("todolist")
+
+        then:
+        noExceptionThrown()
+    }
+
+    def "test getSearchAmount" () {
+        given:
+        final String WORD = "todolist"
+        searchWordRepository.findById(WORD) >> searchWord
+
+        when:
+        searchService.getSearchAmount(WORD)
+
+        then:
+        noExceptionThrown()
+
+        where:
+        searchWord << [Optional.of(new SearchWord("WORD")), Optional.empty()]
+    }
+
+    def "test getNicknameResult" () {
+        when:
+        searchService.getNicknameResult("", "todolist", new PagingRequest(0, 1))
+
+        then:
+        noExceptionThrown()
+    }
+
+    def "test getEmailResult" () {
+        when:
+        searchService.getEmailResult("", "todolist", new PagingRequest(0, 1))
+
+        then:
+        noExceptionThrown()
+    }
+
+    def "test getTemplateResult" () {
+        when:
+        searchService.getTemplateResult("todolist", new PagingRequest(0, 1))
+
+        then:
+        noExceptionThrown()
+    }
+
+}
