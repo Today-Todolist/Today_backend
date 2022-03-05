8015be1e19dca462357b7960b0f34df7282f6422
dohyeon5626 <kodohyeon71@gmail.com>
2022-03-05 15:50:28 +0900

    ♻️ (#46)  - 1:N 쿼리 수정
    list 받아오는 쿼리 최적화

--- src/main/java/todolist/today/today/domain/user/dto/response/UserInfoResponse.java
+++ src/main/java/todolist/today/today/domain/user/dto/response/UserInfoResponse.java
@@ -1,27 +1,19 @@
 package todolist.today.today.domain.user.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import todolist.today.today.domain.user.dto.response.template.UserInfoTemplateResponse;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class UserInfoResponse {
 
     private String nickname;
     private String profile;
     private long friendsAmount;
     private int status;
-
-    @Setter
     private List<UserInfoTemplateResponse> templates;
-
-    public UserInfoResponse(String nickname, String profile, long friendsAmount, int status) {
-        this.nickname = nickname;
-        this.profile = profile;
-        this.friendsAmount = friendsAmount;
-        this.status = status;
-    }
 
 }
