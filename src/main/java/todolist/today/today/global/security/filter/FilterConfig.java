ab74e8cc30f852a45eb7922d5c1ceb16a518494c
dohyeon5626 <kodohyeon71@gmail.com>
2022-04-07 09:50:39 +0900

    ❌ (#75) - 요청 수 제한 기능 삭제
    프록시에서 요청 수 제한을 할 수 있어 서버 자체의 요청 수 제한 기능을 삭제함

--- src/main/java/todolist/today/today/global/security/filter/FilterConfig.java
+++ src/main/java/todolist/today/today/global/security/filter/FilterConfig.java
@@ -7,13 +7,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import todolist.today.today.global.security.service.JwtTokenProvider;
-import todolist.today.today.global.security.service.RequestBucketProvider;
 
 @RequiredArgsConstructor
 public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     private final JwtTokenProvider jwtTokenProvider;
-    private final RequestBucketProvider requestBucketProvider;
     private final ObjectMapper objectMapper;
 
     @Override
@@ -21,11 +19,8 @@ public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilte
         JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
         httpSecurity.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
-        RequestLimitFilter requestLimitFilter = new RequestLimitFilter(requestBucketProvider);
-        httpSecurity.addFilterBefore(requestLimitFilter, JwtTokenFilter.class);
-
         ExceptionHandlerFilter exceptionHandlerFilter = new ExceptionHandlerFilter(objectMapper);
-        httpSecurity.addFilterBefore(exceptionHandlerFilter, RequestLimitFilter.class);
+        httpSecurity.addFilterBefore(exceptionHandlerFilter, JwtTokenFilter.class);
     }
 
 }
